d9c05c45ca783104dc3f8e89019a292e8af76550
crx <570340528@qq.com>
2021-02-13 20:42:32 +0800

    chore:增加注释

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # auto_deploy 简易自动化部署服务
 
-一个通过页面按钮完成一键部署的demo，可选择链接的后端环境地址
+一个通过页面按钮完成一键部署的demo，可选择链接的后端环境地址。
 
-部署服务的后端使用，具体git操作使用child_process执行命令，taskkill负责杀死进程，项目的启动使用http-server
+部署服务前端只有一个简单的html，后端使用 koa，通过 pm2 启动。
+
+服务功能实现通过 child_process 执行命令，使用 taskkill 杀死进程，部署的目标项目使用 http-server 启动。
--- app.js
+++ app.js
@@ -10,18 +10,20 @@ const reloadKoa = new koa();
 
 
 // 这些参数均可从前端配置
-const protocol = "https";
-const projectUrl = "github.com/Jiuto/ding_yapi.git"
-const projectDir = __dirname + '/ding_yapi';
-const staticDir = __dirname + '/static/dist';
-const buildStaticDir = __dirname + '/ding_yapi/client/dist';
+const protocol = "https"; // git clone 命令的协议
+const projectUrl = "github.com/Jiuto/ding_yapi.git"; // git clone 命令的项目地址
+const projectDir = __dirname + '/ding_yapi'; // 目标项目名
+const staticDir = __dirname + '/static/dist'; // 存放打包文件的路径
+const buildStaticDir = __dirname + '/ding_yapi/client/dist'; // 目标项目打包后的文件路径
 
-const git_username='username';
-const git_password='password';
+const git_username='username'; // git 用户名
+const git_password='password'; // git 密码
 
-
+// 目标项目启动的进程
 var subprocess = null;
 
+
+// 删除文件
 function deleteFolder(path) {
     var files = [];
     if (fs.existsSync(path)) { // 目录存在
@@ -42,6 +44,7 @@ function deleteFolder(path) {
     }
 }
 
+// 拷贝文件
 function copyFolder(from, to) {
     var files = [];
     if (fs.existsSync(to)) { // 目录存在
@@ -64,10 +67,10 @@ function copyFolder(from, to) {
 function reloadCode(cmdStr){
     return new Promise(function(resolve, reject) {
         try {
-            child_process.execSync(cmdStr);
-            process.chdir(projectDir)
-            child_process.execSync(`npm install`);
-            child_process.execSync(`npm run build`);
+            child_process.execSync(cmdStr); // 克隆项目
+            process.chdir(projectDir) // 进入子目录
+            child_process.execSync(`npm install`); // 安装依赖
+            child_process.execSync(`npm run build`); // 打包项目
             process.chdir(__dirname) // 返回根目录
             deleteFolder(staticDir) // 删除原静态文件
             copyFolder(buildStaticDir, staticDir) // 拷贝文件
@@ -90,6 +93,7 @@ function changeProxy(cmdStr){
         msg: ''
     };
     return new Promise(function(resolve, reject) {
+        // 项目已启动，则杀死进程
         if(subprocess) {
             (async () => {
                 await taskkill(subprocess.pid);
@@ -119,29 +123,35 @@ app.use(
 
 reloadKoa.use(
     async function(ctx, next){
+        // 解析入参
         const query = ctx.query;
         const branch = decodeURI(query.branch);
         const proxy = decodeURI(query.proxy);
 
+        // 声明两个变量用于存放拉取代码和更新环境两个功能的执行结果
         var rtn1 = {},
             rtn2 = {};
 
-        // 是否拉取代码
+        // 是否重新克隆项目
         if(branch){
+            // 删除上一次拉取的项目
             deleteFolder(projectDir);
-
+            // 构建克隆命令
             let cmdStr = `git clone -b ` + branch + ` ` + protocol + `://` + git_username + `:` + git_password + `@` + projectUrl;
+            // 调用执行克隆命令的方法
             rtn1 = await reloadCode(cmdStr);  
         }
 
 
-        // 是否更新环境
+        // 是否切换代理
         if(proxy){
+            // 构建启动项目命令，将目标项目部署在8002端口
             let cmdStr = `http-server .\\static\\dist -p 8002 --proxy ` + proxy;
-
+            // 调用执行启动项目命令的方法
             rtn2 = await changeProxy(cmdStr);
         }
 
+        // 整合返回结果
         var rtn = {
             code: rtn1.code === 200 || rtn2.code === 200 ? 200 : 500,
             msg: rtn1.msg 
@@ -153,9 +163,9 @@ reloadKoa.use(
                             : 'parameter can nott be empty'
         }
 
+        // 响应
         ctx.status = rtn.code;
         ctx.body = rtn.msg;
-        
     }
 )
 
